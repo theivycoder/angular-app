57d7a1c35b4452ae86a096e5bcac8b871acc3445
Amber Emeny <amber.emeny@griffithuni.edu.au>
2022-12-08 23:25:05 +1000

    updated some post-create component styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -19,6 +23,10 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
