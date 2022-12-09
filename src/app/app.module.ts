0398bc79ba1cb662859fa1ccb0f27319145feddc
Amber Emeny <amber.emeny@griffithuni.edu.au>
2022-12-09 22:40:26 +1000

    added toolbar, post-list component, logic to display lists using ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,21 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +33,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     MatFormFieldModule,
     MatCardModule,
     MatDividerModule,
+    MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
